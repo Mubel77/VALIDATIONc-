c1bffb4b3355c183824144647cb500cc99748fda
Cuellar Mauricio Abel <144746894+Mubel77@users.noreply.github.com>
2024-11-25 12:02:47 -0300

    Update Default6.aspx.cs

--- Uni10Validaciones/Default6.aspx.cs
+++ Uni10Validaciones/Default6.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,4 +11,39 @@ public partial class Default6 : System.Web.UI.Page
     {
 
     }
+    protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (IsValid)
+            {
+
+                Response.Redirect("Default6.aspx");
+
+            }
+
+        }
+}using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default6 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (IsValid)
+            {
+
+                Response.Redirect("Default6.aspx");
+
+            }
+
+        }
 }
